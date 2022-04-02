d2a20d00b4c1eb10086f084d368950d4929d23f4
jonasbark <jonasbark@users.noreply.github.com>
2022-04-02 11:36:18 +0000

    deploy: 6caa217aed197df457bfa8ca91a50b6f1105a6c6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,8 +21,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"index.html": "f4d3c5238f7716dd68b5dea3cc695f0b",
-"/": "f4d3c5238f7716dd68b5dea3cc695f0b"
+"index.html": "601aea2590e9120f5708dc17053bb89e",
+"/": "601aea2590e9120f5708dc17053bb89e"
 };
 
 // The application shell files that are downloaded before a service worker can
